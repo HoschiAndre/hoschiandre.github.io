53c3565d169cb38597ebb4a849bd9b1275596d0a
HoschiAndre <hoschi.andre@gmail.com>
2023-04-19 07:13:02 +0200

    updated from abkisters.de to hoschiandre.github.io

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -16,16 +16,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'abkisters.de'`, () => {
+  it(`should have as title 'hoschiandre.github.io'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('abkisters.de');
+    expect(app.title).toEqual('hoschiandre.github.io');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('abkisters.de app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('hoschiandre.github.io app is running!');
   });
 });
